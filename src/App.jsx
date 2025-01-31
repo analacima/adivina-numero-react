f52f35756e5bc4b611553093c313b7594e628b6f
Ana Lacima <ana.lacima@gmail.com>
2025-01-31 14:27:35 +0100

    preparado para continuar el código

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,19 @@
 import "./App.css";
+import { useState, useRef } from "react";
+
+const number = Math.trunc(Math.random() * 20) + 1;
 
 function App() {
+  const [score, setScore] = useState(20);
+  const [highscore, setHighscore] = useState(0);
+  const inputRef = useRef(null);
+
+  const handleCheck = () => {
+    console.log(inputRef.current.value);
+    const inputNumber = Number(inputRef.current.value);
+    setScore(score - 1);
+  };
+
   return (
     <>
       <header>
@@ -11,16 +24,18 @@ function App() {
       </header>
       <main>
         <section className="left">
-          <input type="number" class="guess" />
-          <button className="btn check">Check!</button>
+          <input type="number" className="guess" ref={inputRef} />
+          <button className="btn check" onClick={handleCheck}>
+            Check!
+          </button>
         </section>
         <section className="right">
           <p className="message">Start guessing...</p>
           <p className="label-score">
-            💯 Score: <span class="score">20</span>
+            💯 Score: <span className="score">{score}</span>
           </p>
           <p className="label-highscore">
-            🥇 Highscore: <span className="highscore">0</span>
+            🥇 Highscore: <span className="highscore">{highscore}</span>
           </p>
         </section>
       </main>
